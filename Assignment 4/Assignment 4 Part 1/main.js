7910c68d968b36040f560a7efd02d335abf7249d
Ovaiota <156240439+Ovaiota@users.noreply.github.com>
2024-03-31 21:07:25 -0400

    corrected errors for variables

--- Assignment 4/Assignment 4 Part 1/main.js
+++ Assignment 4/Assignment 4 Part 1/main.js
@@ -7,24 +7,24 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText ="It was 94 fahrenheit outside, so [:insertx:] went for a walk. When they got to [:inserty:], they stared in horror for a few moments, then [:insertz:]. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
-const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-const insertY = ["the soup kitchen", "Disneyland","the White House"];
-const insertZ =["spontaneously combusted","melted into a puddle on the sidewalk","turned into a slug and crawled away"];
+var storyText ="It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+var insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+var insertY = ["the soup kitchen", "Disneyland","the White House"];
+var insertZ = ["spontaneously combusted","melted into a puddle on the sidewalk","turned into a slug and crawled away"];
 
 randomize.addEventListener('click', result);
 
 function result() {
-    let newStory = storyText; 
+    var newStory = storyText; 
     
-    const xItem = randomValueFromArray(insertX);
-    const yItem = randomValueFromArray(insertY);
-    const zItem = randomValueFromArray(insertZ);
+    var xItem = randomValueFromArray(insertX);
+    var yItem = randomValueFromArray(insertY);
+    var zItem = randomValueFromArray(insertZ);
 
     newStory = newStory
     .replaceAll(":insertx:", xItem)
-    .replace(":inserty:, yItem")
-    .replace(":insertz:, zItem")
+    .replace(":inserty:", yItem)
+    .replace(":insertz:", zItem)
 
   if(customName.value !== '') {
     const name = customName.value;
