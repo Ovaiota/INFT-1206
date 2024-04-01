b725ae95fd37f5d852944ba1c77ae09a7256611c
Ovaiota <156240439+Ovaiota@users.noreply.github.com>
2024-03-31 21:34:58 -0400

    image array & altext
    created an image array for the pictures provided and gave alternative text names

--- Assignment 4/Assignment 4 part 2/gallery-start/gallery-start/main.js
+++ Assignment 4/Assignment 4 part 2/gallery-start/gallery-start/main.js
@@ -5,10 +5,13 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageArray = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altText = ['first image', 'second image','Third image','fourth image', 'fifth image'];
 
 /* Looping through images */
+
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
