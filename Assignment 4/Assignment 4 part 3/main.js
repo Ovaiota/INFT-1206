4b1adbcedbfe037487e682b5e7f3ff6bb7ab62a0
Ovaiota <156240439+Ovaiota@users.noreply.github.com>
2024-04-01 19:16:55 -0400

    loop & Collision detect
    loop calls the function once to get the animation started. Collision detection helps to know when the balls have hit one another.

--- Assignment 4/Assignment 4 part 3/main.js
+++ Assignment 4/Assignment 4 part 3/main.js
@@ -57,7 +57,20 @@ draw() {
     this.x += this.velX;
     this.y += this.velY;
   } 
-
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+  
 }
 const balls = [];
 
@@ -89,4 +102,5 @@ while (balls.length < 25) {
     requestAnimationFrame(loop);
   }
   
+  loop();
 
