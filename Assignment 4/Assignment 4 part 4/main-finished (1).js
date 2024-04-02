1aaf62da59eb501d2d5bcc8076f777161413c9de
Ovaiota <156240439+Ovaiota@users.noreply.github.com>
2024-04-01 20:32:39 -0400

    Evil circle & Ball count
    At the point where you loop through every ball and call the draw(), update(), and collisionDetect() functions for each one, make it so that these functions are only called if the current ball exists.
    Call the evil circle instance's draw(), checkBounds(), and collisionDetect() methods on every iteration of the loop.

--- Assignment 4/Assignment 4 part 4/main-finished (1).js
+++ Assignment 4/Assignment 4 part 4/main-finished (1).js
@@ -163,17 +163,26 @@ while (balls.length < 25) {
   );
 
   balls.push(ball);
-}
+  
+
+const evilBall = new EvilCircle(random(0, width), random(0, height));
 
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, width, height);
 
   for (const ball of balls) {
+    if  (ball.exists) {
     ball.draw();
     ball.update();
     ball.collisionDetect();
   }
+}
+
+ 
+evilBall.draw();
+evilBall.checkBounds();
+evilBall.collisionDetect();
 
   requestAnimationFrame(loop);
 }
